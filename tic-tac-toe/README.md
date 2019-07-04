be8ccd780737ceb9788b3d82b5fd62d43281ffe4
nos-nart <nosnart1510@gmail.com>
2019-07-05 00:26:09 +0700

    [ADD] initial game play

---
+++ tic-tac-toe/README.md
@@ -0,0 +1,10 @@
+* Function play
+- X player go first
+    + Change text of element that event be attached
+    + Disable click event of element just be clicked
+    + Change message display
+- Change turn to O player
+- After every time click => check win || lose
+    + Win => show message who is win?
+    + Disalbed all element
+    + Change background of game (increate opacity)
--- tic-tac-toe/js/main.js
+++ tic-tac-toe/js/main.js
@@ -1,42 +1,63 @@
 $(function() {
     const ELEMENT = {
         $gameBoard: $('.game-board'),
-        $input: $('input'),
+        $input: $('input[name="width"]'),
         $start: $('#start'),
-        $message: $('.message')
+        $message: $('.message span')
     }
     const MSSG = {
         X_TURN: 'X turn',
-        Y_TURN: 'Y turn',
+        O_TURN: 'O turn',
         WIN: 'win!'
     }
 
+    let x_turn = true;
+    // 🇴 ❌
 
     function changeMessage(mssg) {
-
+        ELEMENT.$message.text(mssg)
     }
 
     function initial() {
-        getGrid();
+        changeMessage('Wei, ni hao me?');
+        // Get initial width board
+        ELEMENT.$input.each( function() {
+            if ($(this).prop('checked')) {
+                renderBoard($(this).val())
+            }
+        });
+
+
+    }
+
+    function game() {
+        initial();
+        changeWidth();
         playGame();
     }
 
-    function getGrid() {
+    function changeWidth() {
+        // Event change width board
         ELEMENT.$input.change(function () {
-            ELEMENT.$input.attr('disabled', true)
+            ELEMENT.$input.prop('checked', false);
+            $(this).prop('checked', true)
             renderBoard($(this).val());
+            changeMessage(MSSG.X_TURN);
         })
     }
 
     function playGame() {
-
+        $(document).on('click', '.cell', function(event) {
+            console.log(event)
+        })
     }
 
     function renderBoard(width) {
+        ELEMENT.$gameBoard.empty();
         ELEMENT.$gameBoard.css( "grid-template-columns", `repeat(${width}, 1fr)` );
         for (let i = 0; i < width * width; i++) {
             ELEMENT.$gameBoard.append(
-                $(`<div class="cell" id="${i}"></div>`)
+                $(`<div class="cell grid" id="${i}"><span>${i % 2 === 0 ? '🇴' : '❌'}</span></div>`)
             )
         }
     }
@@ -45,9 +66,5 @@ $(function() {
 
     }
 
-    function showMessage() {
-
-    }
-
-    initial()
+    game()
 });
