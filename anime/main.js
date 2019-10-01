c52b82077e3370da51a7f67b12ebe2397bf64175
nos-nart <nosnart1510@gmail.com>
2019-10-02 01:12:53 +0700

    oo

---
+++ anime/main.js
@@ -0,0 +1,13 @@
+import anime from 'animejs/lib/anime.es.js';
+
+var path = anime.path('.motion-path-demo path');
+
+anime({
+  targets: '.motion-path-demo .el',
+  translateX: path('x'),
+  translateY: path('y'),
+  rotate: path('angle'),
+  easing: 'linear',
+  duration: 5000,
+  loop: true
+});
