30e129b1bd82e47bfd205af224b9c0518fd1aaf9
son.tran <son.tran1@ntq-solution.com.vn>
2020-01-09 15:24:43 +0700

    [~] ll

---
+++ memories-card/src/main.js
@@ -0,0 +1,9 @@
+import './styles/_global.scss';
+import './styles/style.scss';
+import jquery from 'jquery';
+import storage from 'local-storage-fallback';
+window.$ = window.jquery = jquery
+
+$(function () {
+
+})
