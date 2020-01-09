18aa7891e0bfb0f761812af8b223b4afb0e32d0a
nos-nart <nosnart1510@gmail.com>
2020-01-10 00:43:08 +0700

    [+] bb

--- memories-card/src/main.js
+++ memories-card/src/main.js
@@ -2,8 +2,65 @@ import './styles/_global.scss';
 import './styles/style.scss';
 import jquery from 'jquery';
 import storage from 'local-storage-fallback';
+import _ from 'underscore';
+import anime from 'animejs';
 window.$ = window.jquery = jquery
 
 $(function () {
+    const $ELEMENT = {
+        gameBoard: $('.game-board')
+    }
+    const CARD_BACK = './images/back.jpg';
+    const datas = [
+        {url: './images/anivia.png'},
+        {url: './images/ashe.png'},
+        {url: './images/braum.png'},
+        {url: './images/darius.png'},
+        {url: './images/draven.png'},
+        {url: './images/ezreal.png'},
+        {url: './images/fiora.png'},
+        {url: './images/garen.png'},
+        {url: './images/hecarim.png'},
+        {url: './images/heimerdinger.png'},
+        {url: './images/jinx.png'},
+        {url: './images/kalista.png'},
+        {url: './images/karma.png'},
+        {url: './images/katarina.png'},
+        {url: './images/lucian.png'},
+        {url: './images/lux.png'},
+        {url: './images/mighty-poro.png'},
+        {url: './images/poro.png'},
+        {url: './images/shen.png'},
+        {url: './images/teemo.png'},
+        {url: './images/thresh.png'},
+        {url: './images/zed.png'},
+        {url: './images/trymdamere.png'},
+        {url: './images/vladimir.png'},
+        {url: './images/yasuo.png'}
+    ]
 
+    function shuffle(arr) {
+        console.log("TCL: shuffle -> arr", arr)
+        return _.shuffle(arr)
+    }
+
+    function initial() {
+        let temp = shuffle(datas).slice(0, 16);
+        let initArr = shuffle(temp.push(...temp));
+        console.log("TCL: initial -> initArr", initArr)
+    }
+
+    function drawCard(img, data) {
+        let $ele = $('<div class="card" data-index="' + data + '">' +
+            '<div class="front"><div>'+ '<img src="' + img + '"alt="front"/></div></div>' +
+            '<div class="back"><img src="' + CARD_BACK + '"/></div>'
+        +'</div>')
+        return $ele;
+    }
+
+    function runGame() {
+        initial();
+    }
+
+    runGame();
 })
--- memories-card/tailwind.config.js
+++ memories-card/tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
       // Some useful comment
       extend: {
         spacing: {
-         '72': '18rem',
-         '84': '21rem',
-         '96': '24rem',
+          '72': '18rem',
+          '84': '21rem',
+          '96': '24rem',
         }
       },
       colors: {
@@ -68,6 +68,61 @@ module.exports = {
         'opacity': 'opacity',
         'transform': 'transform',
       },
+      transform: { // defaults to this value
+        'none': 'none',
+      },
+      transformOrigin: { // defaults to these values
+        't': 'top',
+        'tr': 'top right',
+        'r': 'right',
+        'br': 'bottom right',
+        'b': 'bottom',
+        'bl': 'bottom left',
+        'l': 'left',
+        'tl': 'top left',
+      },
+      translate: { // defaults to {}
+        '1/2': '50%',
+        'full': '100%',
+        'right-up': ['100%', '-100%'],
+        '3d': ['40px', '-60px', '-130px'],
+      },
+      scale: { // defaults to {}
+        '90': '0.9',
+        '100': '1',
+        '110': '1.1',
+        '-100': '-1',
+        'stretched-x': ['2', '0.5'],
+        'stretched-y': ['0.5', '2'],
+        '3d': ['0.5', '1', '2'],
+      },
+      rotate: { // defaults to {}
+        '90': '90deg',
+        '180': '180deg',
+        '270': '270deg',
+        '3d': ['0', '1', '0.5', '45deg'],
+      },
+      skew: { // defaults to {}
+        '-5': '-5deg',
+        '5': '5deg',
+      },
+      perspective: { // defaults to {}
+        'none': 'none',
+        '250': '250px',
+        '500': '500px',
+        '750': '750px',
+        '1000': '1000px',
+      },
+      perspectiveOrigin: { // defaults to these values
+        't': 'top',
+        'tr': 'top right',
+        'r': 'right',
+        'br': 'bottom right',
+        'b': 'bottom',
+        'bl': 'bottom left',
+        'l': 'left',
+        'tl': 'top left',
+      },
     },
     variants: {
       // Some useful comment
@@ -76,10 +131,41 @@ module.exports = {
       transitionTimingFunction: ['responsive'],
       transitionDelay: ['responsive'],
       willChange: ['responsive'],
+      transform: ['responsive'],
+      transformOrigin: ['responsive'],
+      translate: ['responsive'],
+      scale: ['responsive'],
+      rotate: ['responsive'],
+      skew: ['responsive'],
+      perspective: ['responsive'],
+      perspectiveOrigin: ['responsive'],
+      transformStyle: ['responsive'],
+      backfaceVisibility: ['responsive'],
+      transformBox: ['responsive'],
     },
     plugins: [
       // ...
-      require('tailwindcss-transitions')()
+      require('tailwindcss-transforms')({
+        '3d': true, // defaults to false
+      }),
+      require('tailwindcss-transitions')(),
+      require('tailwindcss-grid')({
+        grids: [2, 3, 5, 6, 8, 10, 12],
+        gaps: {
+          0: '0',
+          2: '0.5rem',
+          4: '1rem',
+          8: '2rem',
+          '4-x': '1rem',
+          '4-y': '1rem',
+        },
+        autoMinWidths: {
+          '16': '4rem',
+          '24': '6rem',
+          '300px': '300px',
+        },
+        variants: ['responsive'],
+      }),
     ]
   }
   
