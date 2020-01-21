d9439ef9ebeef6486374e960819616fe4aaf99d1
nos-nart <nosnart1510@gmail.com>
2020-01-21 23:43:03 +0700

    [+] ll

---
+++ modal-custom/main.js
@@ -0,0 +1,19 @@
+$(function() {
+    (function() {
+        this.Modal = function() {
+            this.closeButton = null;
+            this.modal = null;
+            this.overlay = null;
+
+            var defaults = {
+                className: 'fade-and-drop',
+                closeButton: true,
+                content: '',
+                maxWidth: 600,
+                minWidth: 280,
+                overlay: true
+            }
+        }
+    }())
+    
+})
